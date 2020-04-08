6f2da1cda56f1429656666421cb849a47b93d4f7
lj054551 <lj054551@cerner.com>
2020-04-08 07:54:05 -0500

    new change

--- stats/data.py
+++ stats/data.py
@@ -12,12 +12,11 @@ for game_file in game_files:
 
 
 games = pd.concat(game_frames)
-games.loc[games[id]=='??',['multi5']] = ' '
-print(games)
-# identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
-# identifiers = identifiers.fillna(method='ffill')
-# identifiers.columns['game_id', 'year']
-# games = pd.concat([games, identifiers], axis=1, sort=False)
-# games = games.fillna(' ')
-# games = pd.Categorical(games.loc[:, games['type']])
-# games.head(5)
+games.loc[games['multi5']=='??', 'multi5'] = ''
+identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
+identifiers = identifiers.fillna(method='ffill')
+identifiers.columns = ['game_id', 'year']
+games = pd.concat([games, identifiers], axis=1, sort=False)
+games = games.fillna(' ')
+games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
+print(games.head(5))
